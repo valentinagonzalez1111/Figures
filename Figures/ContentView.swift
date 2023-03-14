fdc8ba777039c7a51d7352f4968021bc639f8869
Valentina <balendi.art@gmail.com>
2023-03-14 11:25:20 +0100

    Bien fait

--- Figures/ContentView.swift
+++ Figures/ContentView.swift
@@ -10,10 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Image(systemName: "person.crop.circle")
+                    .resizable()
+                    .frame(width: 100.0, height: 100.0)
+                    .imageScale(.large)
+                    .foregroundColor(.orange)
+                    .controlSize(/*@START_MENU_TOKEN@*/.large/*@END_MENU_TOKEN@*/)
+                VStack(alignment: .leading) {
+                    Text("Danilo Santana")
+                        .font(.title)
+                        .fontWeight(.regular)
+                        .multilineTextAlignment(.leading)
+                        .frame(height: nil)
+                    Text("Brazil")
+                        .font(.title)
+                        .foregroundColor(Color.gray)
+                        .multilineTextAlignment(.leading)
+                    Text("Racing Driver")
+                        .fontWeight(.semibold)
+                }
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: 30)
+                .padding()
+                .foregroundColor(.gray)
         }
         .padding()
     }
