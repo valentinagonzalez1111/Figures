6303e7f56e2753e4cfb8231662c24306445fe781
Valentina <balendi.art@gmail.com>
2023-03-14 10:28:18 +0100

    Initial Commit

---
+++ Figures/FiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiguresApp.swift
+//  Figures
+//
+//  Created by Apprenant 18 on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
